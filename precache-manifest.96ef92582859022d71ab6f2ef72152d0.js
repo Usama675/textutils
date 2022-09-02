87cc8f4a99c22dd2a120176993731de9e6d73756
Muhammad Usama <lodhiusama2@gmail.com>
2022-09-02 20:07:19 +0500

    Updates

---
+++ precache-manifest.96ef92582859022d71ab6f2ef72152d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c478efc64274b5905627",
+    "url": "/textutils/static/css/main.05d9143d.chunk.css"
+  },
+  {
+    "revision": "c478efc64274b5905627",
+    "url": "/textutils/static/js/main.c478efc6.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "5ddeca23d59ade3a96ef9dde1596e410",
+    "url": "/textutils/index.html"
+  }
+];
